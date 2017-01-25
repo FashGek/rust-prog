7569b47c1e871254f8600fc2b0b7d91ae01903a4
Michael Chen <cwchen@debian>
2017-01-25 20:04:09 +0800

    Adjust language

--- source/conf.py
+++ source/conf.py
@@ -64,7 +64,7 @@ release = '1.0.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'zh-TW'
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -83,7 +83,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -121,7 +121,6 @@ latex_elements = {
     # Additional stuff for the LaTeX preamble.
     #
     'preamble': r"""
-    \usepackage[english]{babel}
     \usepackage{xeCJK}
     \setcounter{secnumdepth}{-1}
     \defaultCJKfontfeatures{AutoFakeBold=6,AutoFakeSlant=.3}
